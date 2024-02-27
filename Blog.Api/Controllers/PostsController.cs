6a7e73676790c4336915c9ecbe5f0e306ffc2b26
Benjamin Rosenthal <brosen8690@gmail.com>
2024-02-26 21:48:21 -0500

    Dal and dummy posts endpoint

---
+++ Blog.Api/Controllers/PostsController.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Dal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class PostsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Post>> GetPosts([FromQuery] int page, [FromQuery] int itemsPerPage)
+        {
+            return new List<Post>
+            {
+                new Post
+                {
+                    Title = "Second Post",
+                    DatePosted = DateTime.Now,
+                    Html = "test"
+                },
+                new Post
+                {
+                    Title = "First Post",
+                    DatePosted = DateTime.Now,
+                    Html = "another test"
+                }
+            };
+        }
+    }
+}
---
+++ Blog.Dal/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Dal.Models
+{
+    public class Post
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Html { get; set; } = string.Empty;
+        public DateTime DatePosted { get; set; }
+    }
+}
